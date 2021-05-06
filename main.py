035b66e8eb16e60aaff9ce6356735f94885363cf
Ekky Sukmawati <>
2021-05-06 17:31:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet(name):
+  print(f"Hi {name}")
+  print(f"Nice to meet you {name}")
+
+name = input("Hi, Whats your name ?")
+greet(name)
